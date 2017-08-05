79ef4fab63b42cdc6a621809153c34f28d5c2c3b
sqrt <sqrt@Mini-de-sqrt.fritz.box>
2017-08-06 00:02:17 +0200

    Refactor locaization

--- apps/cms_web/lib/cms_web/controllers/locale.ex
+++ apps/cms_web/lib/cms_web/controllers/locale.ex
@@ -12,13 +12,18 @@ defmodule CmsWeb.Locale do
     case loc || get_session(conn, :locale) do
       nil     -> conn
       locale  ->
-        Gettext.put_locale(CmsWeb.Gettext, locale)
-        conn |> put_session(:locale, locale)
+        set_locale(conn, locale)
     end
   end
   
   def call(conn, _opts) do
     locale = get_session(conn, :locale) || @default_locale
+    set_locale(conn, locale)
+  end
+  
+  # PRIVATE
+  
+  defp set_locale(conn, locale) do
     Gettext.put_locale(CmsWeb.Gettext, locale)
     conn |> put_session(:locale, locale)
   end
