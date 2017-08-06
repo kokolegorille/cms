d532f2c26665e1aec12008c8f5525164fe5f4b93
sqrt <sqrt@Mini-de-sqrt.fritz.box>
2017-08-06 07:45:00 +0200

    Remove some comments

--- apps/cms_db/lib/cms_db/blogging/post.ex
+++ apps/cms_db/lib/cms_db/blogging/post.ex
@@ -19,21 +19,5 @@ defmodule CmsDb.Blogging.Post do
     post
     |> cast(attrs, @required_fields)
     |> validate_required(@required_fields)
-    # |> strip_unsafe_body(attrs)
   end
-  
-  # # PRIVATE
-  #
-  # defp strip_unsafe_body(model, %{"body" => nil}) do
-  #   model
-  # end
-  #
-  # defp strip_unsafe_body(model, %{"body" => body}) do
-  #   {:safe, clean_body} = Phoenix.HTML.html_escape(body)
-  #   model |> put_change(:body, clean_body)
-  # end
-  #
-  # defp strip_unsafe_body(model, _) do
-  #   model
-  # end
 end
